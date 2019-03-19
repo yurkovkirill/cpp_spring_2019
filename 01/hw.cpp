5fefd6217df091813a243ca7efe890045da2a0c7
Yurkov Kirill <vk1r1ll@yandex.ru>
2019-03-19 18:35:30 +0300

    functionstabs

--- 01/hw.cpp
+++ 01/hw.cpp
@@ -11,25 +11,25 @@ int findindex(int& a,int& b,const int mas[],int size){//a=index(a),b=index(b)
 	int flag=0;
 	int i;
 	for(i = 0;i<size;i++){
-	    	if(mas[i]==a){
-	    		flag = 1;
-	    		a = i;
-	    		break;
-	    	}
-	    }
+			if(mas[i]==a){
+				flag = 1;
+				a = i;
+				break;
+			}
+		}
 	if(flag==0){
 		cout<<0<<endl;
 		return 0;
 	}
 	flag = 0;
 	for(i = 0;i<size;i++){
-	    if(mas[i]==b){
-	    	flag = 1;
-	    }
-	    if((flag==1)&&(mas[i]>b)){
-	    	b = i-1;
-	    	break;
-	    }
+		if(mas[i]==b){
+			flag = 1;
+		}
+		if((flag==1)&&(mas[i]>b)){
+			b = i-1;
+			break;
+		}
 	}
 	if(flag==0){
 		cout<<0<<endl;
@@ -41,25 +41,25 @@ bool Issimple(int num){
 	if(num==1)
 		return false;
 	int Check=floor(sqrt(num));
-		    for(int i = 2; i<=Check; i++){
-		        if (num%i==0){
-		        return false;
-		        }
-		    }
+			for(int i = 2; i<=Check; i++){
+				if (num%i==0){
+				return false;
+				}
+			}
 	return true;
 }
 int countSimple(int a, int b){
 	int count = 0;
-    findindex(a,b,Data,Size);
-    int num;
-    count = 0;
-    bool IsSimple = true;
-    for(int j = a;j<=b;j++){
-    	num = Data[j];
-    	IsSimple = true;
-    	IsSimple = Issimple(num);
-	    if (IsSimple){
-	    	count++;
+	findindex(a,b,Data,Size);
+	int num;
+	count = 0;
+	bool IsSimple = true;
+	for(int j = a;j<=b;j++){
+		num = Data[j];
+		IsSimple = true;
+		IsSimple = Issimple(num);
+		if (IsSimple){
+			count++;
 		}
 	}
 	return count;
@@ -70,7 +70,7 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 	int counter = 0; 
-	int beg;  
+	int beg;
 	int end;
 	int M = (argc-1)/2; 
 	for(int i = 0;i<M;i++){
