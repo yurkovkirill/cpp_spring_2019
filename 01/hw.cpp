8161e66a4c97cfe9b9eedc0e391d82a2e834505c
Yurkov Kirill <vk1r1ll@yandex.ru>
2019-03-19 18:29:46 +0300

    functions2

--- 01/hw.cpp
+++ 01/hw.cpp
@@ -1,5 +1,5 @@
 #include "numbers.dat"
-#include  <cstdlib>
+#include <cstdlib>
 #include <iostream>
 #include <cmath>
 // Data - массив
@@ -18,22 +18,22 @@ int findindex(int& a,int& b,const int mas[],int size){//a=index(a),b=index(b)
 	    	}
 	    }
 	if(flag==0){
-	   	cout<<0<<endl;
-	   	return 0;
+		cout<<0<<endl;
+		return 0;
 	}
 	flag = 0;
 	for(i = 0;i<size;i++){
-	   	if(mas[i]==b){
-	   		flag = 1;
-	   	}
-	   	if((flag==1)&&(mas[i]>b)){
-	   		b = i-1;
-	   		break;
-	   	}
+	    if(mas[i]==b){
+	    	flag = 1;
+	    }
+	    if((flag==1)&&(mas[i]>b)){
+	    	b = i-1;
+	    	break;
+	    }
 	}
 	if(flag==0){
-	 	cout<<0<<endl;
-	   	return 0;
+		cout<<0<<endl;
+		return 0;
 	}
 	return 1;
 }
@@ -69,16 +69,16 @@ int main(int argc, char* argv[])
 	if(((argc-1)%2!=0)||(argc==1)){
 		return -1;
 	}
-	int counter = 0;    
+	int counter = 0; 
 	int beg;  
 	int end;
 	int M = (argc-1)/2; 
 	for(int i = 0;i<M;i++){
 		beg = atoi(argv[1+2*i]); 
-    	end = atoi(argv[2+2*i]);
-    	counter = countSimple(beg,end);
+		end = atoi(argv[2+2*i]);
+		counter = countSimple(beg,end);
 		//std::cout<<"count of simple "<<counter<<std::endl;
 		std::cout<<counter<<std::endl;
 	}
-    return 0;
+	return 0;
 }
