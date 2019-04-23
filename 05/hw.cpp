d0d1112ba593cb17ba2fe2fd0087d0cfb8eab6b2
Yurkov Kirill <vk1r1ll@yandex.ru>
2019-04-23 15:44:18 +0300

    upd 05

--- 05/hw.cpp
+++ 05/hw.cpp
@@ -6,25 +6,25 @@ using namespace std;
 
 std::atomic<bool> check;
 
-void ping_pong(const char * string, bool value)
+void ping_pong(const char* string, bool value)
 {
-    for(int i = 0; i < count; i++)
-    {
-        while(check == value);//ждем другой поток
-        cout << string << i << endl;
-        check = value;//переход в другой поток
+    for (int i = 0; i < count; i++) {
+        while (check == value)
+            ; //ждем другой поток
+        cout << string << endl;
+        check = value; //переход в другой поток
     }
 }
 
 int main()
 {
     check = true;
-   	
+
     std::thread t1(ping_pong, "ping", false);
     std::thread t2(ping_pong, "pong", true);
-    
+
     t1.join();
     t2.join();
-    
+
     return 0;
 }
